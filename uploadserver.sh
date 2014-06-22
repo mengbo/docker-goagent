98d205e6e4169c3403987193b54e72bbba542d23
Meng Bo <mengbo@lnu.edu.cn>
2014-06-22 14:51:01 +0800

    Change default env in scripts.

--- uploadserver.sh
+++ uploadserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-GOAGENT_GAE_PASSWORD=${GOAGENT_GAE_PASSWORD:-"docker-goagent"}
+GOAGENT_GAE_PASSWORD=${GOAGENT_GAE_PASSWORD}
 
 sed "s/^__password__ =.*$/__password__ = '$GOAGENT_GAE_PASSWORD'/" \
 	/usr/local/src/goagent/server/gae/gae.py \
