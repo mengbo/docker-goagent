7e49d7bac760010c80bf3cca75086133bfd2b300
Meng Bo <mengbo@lnu.edu.cn>
2014-06-24 08:46:23 +0800

    Fix the bug of env var.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-docker pull mengbo/docker-goagent
+#docker pull mengbo/docker-goagent
+
+CONTAINER=`docker ps |grep docker-goagent | cut -f 1 -d ' '`
+for c in $ALL_CONTAINER; do
+	docker stop $c
+done
 
 ALL_CONTAINER=`docker ps -a |grep docker-goagent | cut -f 1 -d ' '`
 for c in $ALL_CONTAINER; do
@@ -25,13 +30,13 @@ read GOAGENT_GAE_PASSWORD
 
 DOCKER_VOLUME="-v /usr/local/share/ca-certificates:/usr/local/share/ca-certificates"
 
-DOCKER_RUN_ENV="-e GOAGENT_LISTEN_USERNAME=\"${GOAGENT_LISTEN_USERNAME}\" \
-	       -e GOAGENT_LISTEN_PASSWORD=\"${GOAGENT_LISTEN_PASSWORD}\" \
-	       -e GOAGENT_GAE_APPID=\"${GOAGENT_GAE_APPID}\" \
-	       -e GOAGENT_GAE_PASSWORD=\"${GOAGENT_GAE_PASSWORD}\""
+DOCKER_RUN_ENV="-e GOAGENT_LISTEN_USERNAME=${GOAGENT_LISTEN_USERNAME} \
+	       -e GOAGENT_LISTEN_PASSWORD=${GOAGENT_LISTEN_PASSWORD} \
+	       -e GOAGENT_GAE_APPID=${GOAGENT_GAE_APPID} \
+	       -e GOAGENT_GAE_PASSWORD=${GOAGENT_GAE_PASSWORD}"
 
 docker run --name docker-goagent --dns=127.0.0.1 -d \
 	-p 53:53 -p 53:53/udp -p 8087:8087 \
-	$DOCKER_VOLUME $DOCKER_RUN_ENV mengbo/docker-goagent
+	${DOCKER_VOLUME} ${DOCKER_RUN_ENV} mengbo/docker-goagent
 sleep 10
 docker cp docker-goagent:/opt/goagent/local/CA.crt .
